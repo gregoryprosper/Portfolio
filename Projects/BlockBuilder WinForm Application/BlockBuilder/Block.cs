c28fa3de7455ac0bb4186c704801dff50c51ddc4
gregoryprosper <gregoryprosper@fau.edu>
2015-09-03 13:47:46 -0400

    update
    added BlockBuilder
    renamed folders

---
+++ Projects/BlockBuilder WinForm Application/BlockBuilder/Block.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuilder
+{
+    class Block
+    {
+        private Rectangle bounds;
+        public Rectangle Bounds
+        {
+            get{return bounds;}
+        }
+        private Color color;
+        public Color Color
+        {
+            get{return color;}
+        }
+        public static int size = 20;
+
+        public Block()
+        {
+            this.bounds = new Rectangle(new Point(size, size), new Size(size, size));
+            this.color = Color.Black;
+        }
+
+        public Block(int x, int y, Color color)
+        {
+            this.bounds = new Rectangle(new Point(x, y), new Size(size, size));
+            this.color = color;
+        }
+
+        public void move(int x, int y, Rectangle enclosedBounds)
+        {
+            if(bounds.X + bounds.Width + x <= enclosedBounds.Width && bounds.X + x >= enclosedBounds.X){
+                bounds.X += x;
+            }
+
+            if (bounds.Y + bounds.Height + y <= enclosedBounds.Height && bounds.Y + y >= enclosedBounds.Y)
+            {
+                bounds.Y += y;
+            }
+        }
+
+        public bool containsPoint(Point point)
+        {
+            if (point.X >= bounds.X && point.X <= bounds.X + bounds.Width && point.Y >= bounds.Y && point.Y <= bounds.Y + bounds.Height)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Projects/BlockBuilder WinForm Application/BlockBuilder/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace BlockBuilder
+{
+    partial class BlockBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.colorChangeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainPanel.Location = new System.Drawing.Point(9, 10);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(750, 674);
+            this.mainPanel.TabIndex = 0;
+            this.mainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel_Paint);
+            this.mainPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseClick);
+            this.mainPanel.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDoubleClick);
+            this.mainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseDown);
+            this.mainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseMove);
+            this.mainPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainPanel_MouseUp);
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.colorPanel.Location = new System.Drawing.Point(818, 324);
+            this.colorPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(83, 82);
+            this.colorPanel.TabIndex = 1;
+            this.colorPanel.Click += new System.EventHandler(this.colorPanel_Click);
+            // 
+            // colorChangeButton
+            // 
+            this.colorChangeButton.Location = new System.Drawing.Point(963, 338);
+            this.colorChangeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.colorChangeButton.Name = "colorChangeButton";
+            this.colorChangeButton.Size = new System.Drawing.Size(66, 50);
+            this.colorChangeButton.TabIndex = 2;
+            this.colorChangeButton.Text = "Get Random Color";
+            this.colorChangeButton.UseVisualStyleBackColor = true;
+            this.colorChangeButton.Click += new System.EventHandler(this.colorChangeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(810, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 44);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Block Builder";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BlockBuilder.Properties.Resources.blocks;
+            this.pictureBox1.Location = new System.Drawing.Point(818, 119);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 157);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AllowFullOpen = false;
+            this.colorDialog1.SolidColorOnly = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(818, 523);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(212, 41);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear Canvas";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(818, 569);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(212, 41);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Save Image To Desktop";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(858, 473);
+            this.trackBar.Maximum = 50;
+            this.trackBar.Minimum = 10;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(172, 45);
+            this.trackBar.SmallChange = 5;
+            this.trackBar.TabIndex = 8;
+            this.trackBar.TickFrequency = 5;
+            this.trackBar.Value = 10;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(815, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Size:";
+            // 
+            // BlockBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 700);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.colorChangeButton);
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.mainPanel);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(1092, 739);
+            this.Name = "BlockBuilder";
+            this.Text = "Block Builder";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Button colorChangeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
