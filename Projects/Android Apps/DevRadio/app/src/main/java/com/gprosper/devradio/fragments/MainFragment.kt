fc519c3e645fd9953a6465a2a27e466d96f7a5ad
Greg <gregoryprosper@gmail.com>
2018-02-14 23:11:33 -0500

    Fragment fix

--- Projects/Android Apps/DevRadio/app/src/main/java/com/gprosper/devradio/fragments/MainFragment.kt
+++ Projects/Android Apps/DevRadio/app/src/main/java/com/gprosper/devradio/fragments/MainFragment.kt
@@ -3,6 +3,7 @@ package com.gprosper.devradio.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,6 +19,7 @@ import com.gprosper.devradio.R
 class MainFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainFragment", "onCreate")
         super.onCreate(savedInstanceState)
         if (arguments != null) {
         }
@@ -25,15 +27,26 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        Log.d("MainFragment", "onCreateView")
         return inflater!!.inflate(R.layout.fragment_main, container, false).apply {
             activity.supportFragmentManager.let {
-                val stationFragment1 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_FEATURED)
-                val stationFragment2 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_RECENT)
-                val stationFragment3 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_PARTY)
+                var stationFragment1 = it.findFragmentById(R.id.container_top_row)
+                if (stationFragment1 == null){
+                    stationFragment1 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_FEATURED)
+                    it.beginTransaction().add(R.id.container_top_row, stationFragment1).commit()
+                }
 
-                it.beginTransaction().add(R.id.container_top_row, stationFragment1).commit()
-                it.beginTransaction().add(R.id.container_middle_row, stationFragment2).commit()
-                it.beginTransaction().add(R.id.container_bottom_row, stationFragment3).commit()
+                var stationFragment2 = it.findFragmentById(R.id.container_middle_row)
+                if (stationFragment2 == null){
+                    stationFragment2 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_RECENT)
+                    it.beginTransaction().add(R.id.container_middle_row, stationFragment2).commit()
+                }
+
+                var stationFragment3 = it.findFragmentById(R.id.container_bottom_row)
+                if (stationFragment3 == null){
+                    stationFragment3 = StationsFragment.newInstance(StationsFragment.STATION_TYPE_PARTY)
+                    it.beginTransaction().add(R.id.container_bottom_row, stationFragment3).commit()
+                }
             }
         }
     }
